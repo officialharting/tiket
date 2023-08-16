0392267665c450de954f5b612843082e338609d7
mnasdar <mnasdar@gmail.com>
2023-08-16 16:13:39 +0800

    Menambahkan Template

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,56 @@
+<!-- header begin -->
+<header class="transparent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="de-flex sm-pt10">
+                    <div class="de-flex-col">
+                        <div class="de-flex-col">
+                            <!-- logo begin -->
+                            <div id="logo">
+                                <a href="02_djfest-index.html">
+                                    <img alt="" src="images-dj/logo-light.png" />
+                                </a>
+                            </div>
+                            <!-- logo close -->
+                        </div>
+                        <div class="de-flex-col">
+                        </div>
+                    </div>
+                    <div class="de-flex-col header-col-mid">
+                        <!-- mainmenu begin -->
+                        <ul id="mainmenu">
+                            <li><a href="#de-carousel">Home</a>
+                                <ul>
+                                    <li><a href="index.html">Rockfest</a></li>
+                                    <li><a href="02_djfest-index.html">DJFest</a></li>
+                                    <li><a href="03_electrofest-index.html">Electrofest</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#section-artists">Artists</a></li>
+                            <li><a href="#section-schedule">Schedule</a></li>
+                            <li><a href="#section-tickets">Tickets</a></li>
+                            <li><a href="#section-gallery">Gallery</a></li>
+                            <li><a href="index-dj.html">Pages</a>
+                                <ul>
+                                    <li><a href="02_djfest-blog.html">Blog</a></li>
+                                    <li><a href="02_djfest-contact.html">Contact</a></li>
+                                    <li><a href="02_djfest-gallery.html">Gallery</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="de-flex-col">
+                        <div class="menu_side_area">
+                            <a href="#section-tickets" class="btn-main"><i class="fa fa-sign-in"></i><span>Buy
+                                    Ticket</span></a>
+                            <span id="menu-btn"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- header close -->
---
+++ resources/views/master-layouts/client.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Rockfest - Music Festival Event, DJ Concert and Night Club Website Template</title>
+    <link rel="icon" href="{{ asset('client/images-dj/icon.png') }}" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Rockfest - Music Festival Event, DJ Concert and Night Club Website Template" name="description" />
+    <meta content="" name="keywords" />
+    <meta content="" name="author" />
+    <!-- CSS Files
+    ================================================== -->
+    <link id="bootstrap" href="{{ asset('client/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-grid" href="{{ asset('client/css/bootstrap-grid.min.css') }}" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-reboot" href="{{ asset('client/css/bootstrap-reboot.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/animate.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/owl.carousel.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/owl.theme.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/owl.transitions.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/magnific-popup.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/jquery.countdown.css') }}" rel="stylesheet" type="text/css" />
+    <link id="mdb" href="{{ asset('client/css/mdb.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/style.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/de-dj.css') }}" rel="stylesheet" type="text/css" />
+    <!-- color scheme -->
+    <link id="colors" href="{{ asset('client/css/colors/scheme-02.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/css/coloring.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="dark-scheme">
+
+    {{ $slot }}
+
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="{{ asset('client/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('client/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('client/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('client/js/wow.min.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.isotope.min.js') }}"></script>
+    <script src="{{ asset('client/js/easing.js') }}"></script>
+    <script src="{{ asset('client/js/owl.carousel.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('client/js/enquire.min.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.plugin.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.countTo.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.countdown.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.lazy.min.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.lazy.plugins.min.js') }}"></script>
+    <script src="{{ asset('client/js/mdb.min.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.countdown.js') }}"></script>
+    <script src="{{ asset('client/js/countdown-custom.js') }}"></script>
+    <script src="{{ asset('client/js/cookit.js') }}"></script>
+    <script src="{{ asset('client/js/designesia.js') }}"></script>
+
+<!-- COOKIES PLUGIN  -->
+     <script>
+      $(document).ready(function() {
+        $.cookit({
+          backgroundColor: '#cdff6b',
+          messageColor: '#000000',
+          linkColor: '#000000',
+          buttonColor: '#000000',
+          messageText: "This website uses cookies to ensure you get the best experience on our website.",
+          linkText: "Learn more",
+          linkUrl: "02_djfest-index.html",
+          buttonText: "I accept",
+        });
+      });
+    </script>
+</body>
+
+</html>
